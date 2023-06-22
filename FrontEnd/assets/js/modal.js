395b61485928e9c754719f5156556dd3fc718294
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-06-22 10:17:17 +0200

    Mise en place des évènements au clic pour passer de galerie photo à ajout photo et inversement

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -14,7 +14,28 @@
 
     // Fonction liée au clic des déclencheurs ouverture/fermeture modale
     function openAndCloseModal() {
-        modalContainer.classList.toggle("active");
-        modalOne.style.display = "flex";
-        modalTwo.style.display = "none";
+        modalContainer.classList.toggle("active"); // Ajoute la class active pour afficher la modale
+        modalOne.style.display = "flex"; // Affiche la modale "Galerie photo"
+        modalTwo.style.display = "none"; // Masque la modale "Ajout photo"
+    }
+
+// Evènement pour accéder à la deuxième modale ou revenir à la première modale
+    // Sélection du bouton "Ajouter photo"
+    const nextModal = document.querySelector(".modal__one-addbutton");
+    // Evènement sur le bouton  "Ajouter photo" au clic
+    nextModal.addEventListener("click", openNextModal);
+    // Fonction lié au bouton "Ajouter photo"
+    function openNextModal() {
+        modalOne.style.display = "none"; // Masque modale "Galerie photo"
+        modalTwo.style.display = "flex"; // Affiche modale "Ajout photo"
+    }
+
+    // Sélection de l'icône de retour
+    const returnModal = document.querySelector(".return__modal-one");
+    // Evènement au clic sur l'icône
+    returnModal.addEventListener("click", returnFirstModal);
+    // Fonction liée à l'icône de retour
+    function returnFirstModal() {
+        modalOne.style.display = "flex"; // Affiche modale "Galerie photo"
+        modalTwo.style.display = "none"; // Masque modale "Ajout photo"
     }
